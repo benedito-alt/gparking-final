68f606b2c8c3edf3e9981fc8e8264f1cddd949dc
benedito-alt <benedito@ba.estudante.senai.br>
2026-05-11 15:24:29 -0300

    Correção das credenciais e URL do Supabase

--- public/js/db.js
+++ public/js/db.js
@@ -1,19 +1,9 @@
-const { createClient } = require('@supabase/supabase-js');
-
-// Essas informações você pega em: Settings > API no painel do Supabase
+// FORMATO PARA O NAVEGADOR (FRONTEND)
 const supabaseUrl = 'https://csaqfeivvtmtjlnwuxql.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzYXFmZWl2dnRtdGpsbnd1eHFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTE4MTMsImV4cCI6MjA5MzgyNzgxM30.kBm3IARUdM_9PxDOAejc3Svg0d21ya0jiF-dpje5eOE';
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+// Usamos a variável global que o script do Supabase cria no HTML
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-// Exportamos o "supabase" para usar no server.js
-module.exports = supabase;
-
-// No objeto de dados que você envia para o banco:
-const dadosUsuario = {
-    id: user.id, // UUID do Auth
-    nome: nome,
-    telefone: telefone,
-    tipo_usuario: tipo,
-    faculdade_id: document.getElementById("faculdade").value // A nova linha mágica
-};
+// Não usamos module.exports no navegador! 
+// A variável _supabase já ficará disponível para os outros arquivos .js
